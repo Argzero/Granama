f9700d5742b1213343b56324b576db212e333721
Steven Sucy <sms6462@g.rit.edu>
2014-11-22 18:52:21 -0500

    Finished converting weapons

--- js/weapon/weapons.js
+++ js/weapon/weapons.js
@@ -161,6 +161,49 @@ var weapon = {
     },
 
     /**
+     * A weapon that slashes a sword in front of the user
+     * <ul>
+     *     <li>{number} rate        - the number of frames between shots</li>
+     *     <li>{number} radius      - the radius of the swing arc</li>
+     *     <li>{number} angle       - the angle to start the swing at</li>
+     *     <li>{number} arc         - the angle of the swing arc</li>
+     *     <li>{string} sprite      - the name of the sword image</li>
+     *     <li>{number} knockback   - the amount of knockback to apply</li>
+     *     <li>{number} [lifesteal] - the amount of lifesteal to apply</li>
+     *     <li>{number} [range]     - the range of the bullet</li>
+     *     <li>{number} [dx]        - horizontal position offset</li>
+     *     <li>{number} [dy]        - vertical position offset</li>
+     *     <li>{number} [delay]     - the delay before shooting while in range</li>
+     * </ul>
+     *
+     * @param {Object} data - weapon data
+     */
+    doubleSword: function(data) {
+        if (weapon.checkTime(data, this.isInRange(data.range || 150))) {
+            if (data.dx) {
+                var m = this.right ? 1 : -1;
+                data.dx = Math.abs(data.dx) * m;
+            }
+            var pos = weapon.getPosition(this, data);
+            var side = this.right ? 'Right' : 'Left';
+            this.right = !this.right;
+            var sword = new Sword(
+                data.sprite + side,
+                this,
+                pos,
+                data.radius,
+                data.angle,
+                data.damage,
+                data.arc * m,
+                data.knockback,
+                0
+            );
+            data.list.push(sword);
+            this.sword = false;
+        }
+    },
+
+    /**
      * A flamethrower weapon with expanding fire projectiles
      * <ul>
      *     <li>{number}  rate          - the number of frames between shots</li>
@@ -192,6 +235,37 @@ var weapon = {
                 data.range * 1.5
             );
             data.list.push(fire);
+        }
+    },
+
+    /**
+     * A weapon that slashes a sword in front of the user (usable only by fist boss)
+     * <ul>
+     *     <li>{number} rate       - the number of frames between shots</li>
+     *     <li>{number} speed      - how fast the fist travels</li>
+     *     <li>{number} [range]    - the range of the fist</li>
+     *     <li>{number} [delay]    - the delay before shooting while in range</li>
+     *     <li>{number} [duration] - how long the fist stays still</li>
+     * </ul>
+     *
+     * @param {Object} data - weapon data
+     */
+    fist: function(data) {
+        if (weapon.checkTime(data, this.isInRange(data.range || 150))) {
+            var tx = (data.side ? -1 : 1) * (5 + this.sprite.width / 2 + this.rightFistImg.width / 2);
+            var fist = new Fist(
+                this,
+                new Vector(tx + this.x, this.y),
+                this.forward().multiply(data.speed, data.speed),
+                this.rotation.clone(),
+                data.damage,
+                data.range * 1.5,
+                data.duration || 120,
+                data.side ? 'Right' : 'Left'
+            );
+            gameScreen.enemyManager.bullets.push(fist);
+            this[(data.side ? 'right' : 'left') + 'Fist'] = false;
+            data.side = !data.side;
         }
     },
 
@@ -337,14 +411,15 @@ var weapon = {
     /**
      * A weapon that drops mines
      * <ul>
-     *     <li>{number}  rate          - the number of frames between shots</li>
-     *     <li>{number}  damage        - the damage dealt by the bullet</li>
-     *     <li>{number}  range         - the range of the bullet</li>
-     *     <li>{string}  [type]        - the type of mine</li>
-     *     <li>{number}  [speed]       - the speed of the bullet</li>
-     *     <li>{number}  [dx]          - horizontal position offset</li>
-     *     <li>{number}  [dy]          - vertical position offset</li>
-     *     <li>{number}  [delay]       - the delay before shooting while in range</li>
+     *     <li>{number}  rate       - the number of frames between shots</li>
+     *     <li>{number}  damage     - the damage dealt by the bullet</li>
+     *     <li>{number}  range      - the range of the bullet</li>
+     *     <li>{string}  [type]     - the type of mine</li>
+     *     <li>{number}  [speed]    - the speed of the bullet</li>
+     *     <li>{number}  [dx]       - horizontal position offset</li>
+     *     <li>{number}  [dy]       - vertical position offset</li>
+     *     <li>{number}  [delay]    - the delay before shooting while in range</li>
+     *     <li>{number}  [duration] - the lifespan of the mine
      * </ul>
      *
      * @param {Object} data - weapon data
@@ -356,6 +431,74 @@ var weapon = {
             enemyManager.mines.push(mine);
             if (data.duration) {
                 mine.lifespan = data.duration;
+            }
+        }
+    },
+
+    /**
+     * A weapon that applies another weapon after charging up over an interval. Along with the values below,
+     * you should add the values for the sub weapon to the weapon data. Any values used by this
+     * that are also used by the sub weapon are ignored in the sub weapon.
+     * <ul>
+     *     <li>{function} subWeapon  - the weapon to fire</li>
+     *     <li>{number}   rate       - the charge up time in frames</li>
+     *     <li>{number}   duration   - the firing interval time in frames</li>
+     *     <li>{number}   range      - the range of the weapon</li>
+     *     <li>{number}   [interval] - the delay between each shot during the firing interval</li>
+     *     <li>{boolean}  [initial]  - whether or not to skip the first charging time</li>
+     *     <li>{number}   [bullets]  - the number of bullets to fire per shot</li>
+     *     <li>{number}   [delay]    - the delay before firing outside of charging up</li>
+     * </ul>
+     *
+     * @param {Object} data - weapon data
+     */
+    rail: function(data) {
+
+        // Initialize values
+        data.subWeapon = data.subWeapon || weapon.gun;
+        if (data.delayTimer === undefined) data.delayTimer = 0;
+
+        // Charge up when in range
+        if (this.isInRange(data.range) || data.cd < 0) {
+            if (data.delay && data.delayTimer < data.delay) {
+                data.delayTimer++;
+                return;
+            }
+            data.cd--;
+
+            // Fire when charged up
+            if (data.cd < 0) {
+                if (data.intervalTimer > 1) {
+                    data.intervalTimer--;
+                    return;
+                }
+                this.subWeapon = data.subWeapon;
+                var tempCd = data.cd;
+                var tempDelay = data.delay;
+                data.delay = 0;
+                for (var i = 0; i < (data.bullets || 1); i++) {
+                    this.subWeapon(data);
+                    data.cd = tempCd;
+                }
+                data.delay = tempDelay;
+                data.intervalTimer = data.interval;
+
+                // "Overheating" resets the charge countdown
+                if (data.cd < -data.duration) {
+                    data.cd = data.rate;
+                }
+            }
+        }
+
+        // Discharge when unable to attack
+        else if (data.cd <= data.rate) {
+            if (data.initial) {
+                if (data.cd > 0) {
+                    data.cd--;
+                }
+            }
+            else {
+                data.cd += data.discharge;
             }
         }
     },
@@ -402,6 +545,54 @@ var weapon = {
             );
             data.list.push(rocket);
             weapon.spread(rocket, data);
+        }
+    },
+
+    /**
+     * A weapon that emits a shockwave that expands outward
+     * <ul>
+     *     <li>{number}  rate        - the number of frames between shots</li>
+     *     <li>{number}  damage      - the damage dealt by the bullet</li>
+     *     <li>{number}  range       - the range of the bullet</li>
+     *     <li>{number}  start       - the starting angle of the shockwave</li>
+     *     <li>{number}  end         - the ending angle of the shockwave</li>
+     *     <li>{number}  radius      - the starting radius of the arc</li>
+     *     <li>{number}  [thickness] - the width of the shockwave</li>
+     *     <li>{string}  [color1]    - the primary color of the shockwave</li>
+     *     <li>{string}  [color2]    - the secondary color of the shockwave</li>
+     *     <li>{number}  [speed]     - the speed of the bullet</li>
+     *     <li>{number}  [knockback] - the amount of knockback</li>
+     *     <li>{number}  [dx]        - horizontal position offset</li>
+     *     <li>{number}  [dy]        - vertical position offset</li>
+     *     <li>{number}  [delay]     - the delay before shooting while in range</li>
+     *     <li>{boolean} [alternate] - whether or not to alternate sides</li>
+     * </ul>
+     *
+     * @param {Object} data - weapon data
+     */
+    shockwave: function(data) {
+        if (weapon.checkTime(data, this.isInRange(data.range))) {
+            var pos = weapon.getPosition(this, data);
+            var shockwave = Shockwave(
+                this,
+                data.color1 || '#ff9933',
+                data.color2 || '#f70',
+                pos,
+                data.speed || 5,
+                data.start + this.angle,
+                data.end + this.angle,
+                data.radius,
+                data.thickness || 20,
+                data.damage,
+                data.range,
+                data.knockback
+            );
+            data.list.push(shockwave);
+            if (data.alternate) {
+                var temp = Math.PI - data.end;
+                data.end = Math.PI - data.start;
+                data.start = temp;
+            }
         }
     },
 
@@ -489,6 +680,7 @@ var weapon = {
      *     <li>{number} arc         - the angle of the swing arc</li>
      *     <li>{number} knockback   - the amount of knockback to apply</li>
      *     <li>{number} [lifesteal] - the amount of lifesteal to apply</li>
+     *     <li>{string} [sprite]    - name of the sword image</li>
      *     <li>{number} [range]     - the range of the bullet</li>
      *     <li>{number} [dx]        - horizontal position offset</li>
      *     <li>{number} [dy]        - vertical position offset</li>
@@ -514,6 +706,27 @@ var weapon = {
             );
             data.list.push(sword);
             this.sword = false;
+        }
+    },
+
+    /**
+     * A weapon that drops mines
+     * <ul>
+     *     <li>{number} rate    - the number of frames between shots</li>
+     *     <li>{number} damage  - the damage dealt by the bullet</li>
+     *     <li>{number} range   - the range of the bullet</li>
+     *     <li>{number} health  - the amount of health the turret has</li>
+     *     <li>{number} [dx]    - horizontal position offset</li>
+     *     <li>{number} [dy]    - vertical position offset</li>
+     *     <li>{number} [delay] - the delay before shooting while in range</li>
+     * </ul>
+     *
+     * @param {Object} data - weapon data
+     */
+    turret: function(data) {
+        if (weapon.checkTime(data, this.isInRange(data.range))) {
+            var pos = weapon.getPosition(this, data);
+            gameScreen.enemyManager.turrets.push(new Turret(pos, data.damage, data.health));
         }
     }
 };
