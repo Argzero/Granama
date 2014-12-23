8a1b01f9ab5f38711e2e9c427e86327d80aeddd7
Steven Sucy <sms6462@g.rit.edu>
2014-12-22 19:29:48 -0500

    Added trails to buff particles

--- js/buffManager.js
+++ js/buffManager.js
@@ -9,6 +9,11 @@ var BUFF = {
 };
 
 var BP_ROT = Vector(Math.cos(0.05), Math.sin(0.05));
+var TRAIL_LENGTH = 3;
+
+// 6 is the number of buffs
+// 4 is the number of trail particles + 1
+var BT_ROT = Vector(Math.cos(Math.PI * 2 / (6 * (TRAIL_LENGTH + 1))), -Math.sin(Math.PI * 2 / (6 * (TRAIL_LENGTH + 1))));
 
 /**
  * A manager for buff particles
@@ -71,6 +76,7 @@ BuffManager.prototype.draw = function()
 	var sin = Math.sin(angle);
 	
 	var dir = Vector(this.rotation.x, this.rotation.y);
+	var trailDir = Vector(0, 0);
 	
 	// Draw active particles
 	for (var i = 0; i < this.buffs.length; i++) 
@@ -78,7 +84,19 @@ BuffManager.prototype.draw = function()
 		if (this.buffs[i])
 		{
 			var img = GetImage('buff' + this.imgs[i]);
+			var trail = GetImage('buff' + this.imgs[i] + 'Trail');
 			canvas.drawImage(img, dir.x - img.width / 2, dir.y - img.height / 2);
+			
+			trailDir.x = dir.x;
+			trailDir.y = dir.y;
+			for (var j = 0; j < TRAIL_LENGTH; j++)
+			{
+				canvas.globalAlpha = (TRAIL_LENGTH - j) / (TRAIL_LENGTH + 1);
+				trailDir.Rotate(BT_ROT.x, BT_ROT.y);
+				canvas.drawImage(trail, trailDir.x - trail.width / 2, trailDir.y - trail.height / 2);
+			}
+			canvas.globalAlpha = 1;
+			
 			dir.Rotate(cos, sin);
 		}
 	}
