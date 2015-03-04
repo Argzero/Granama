94af85b1fa70d5e4fa58b3a2819f178de7311be7
Steven Sucy <sms6462@g.rit.edu>
2015-03-04 10:50:21 -0500

    Started working on royal hydra

--- js/robot/boss/hydra.js
+++ js/robot/boss/hydra.js
@@ -126,139 +126,299 @@ HydraBoss.prototype.onPreDraw = function() {
     }
 };
 
-// /**
- // * Boss that uses a minigun, mines, and rockets to fight
- // *
- // * @param {number} x - horizontal starting position
- // * @param {number} y - vertical starting position
- // */
-// extend('DragonBoss', 'Boss');
-// function DragonBoss(x, y) {
-    // this.super(
-        // /* Sprite      */ 'bossDragonHead', 
-        // /* Position    */ x, y, 
-        // /* Type        */ Robot.BOSS, 
-        // /* Health      */ 750 * Enemy.pow(1.4) * players.length,
-        // /* Speed       */ 4 + 0.3 * gameScreen.bossCount,
-        // /* Move Range  */ 300,
-        // /* Experience  */ Enemy.DRAGON_EXP,
-        // /* Stat Rank   */ Enemy.DRAGON_ENEMY,
-        // /* Pattern Min */ 600,
-        // /* Pattern Max */ 750
-    // );
+/**
+ * Umm...it's the hydra...it explains itself...
+ *
+ * @param {number} x - horizontal starting position
+ * @param {number} y - vertical starting position
+ */
+extend('RoyalHydra', 'Boss');
+function RoyalHydra(x, y) {
+    this.super(
+        /* Sprite      */ 'hydraRoyalBody', 
+        /* Position    */ x, y, 
+        /* Type        */ Robot.BOSS, 
+        /* Health      */ 1200 * Enemy.pow(1.4) * players.length,
+        /* Speed       */ 8,
+        /* Move Range  */ 750,
+        /* Experience  */ Enemy.HYDRA_EXP,
+        /* Stat Rank   */ Enemy.HYDRA_ENEMY,
+        /* Pattern Min */ 720,
+        /* Pattern Max */ 900
+    );
     
-    // this.pierceDamage = 0.4;
-    // this.turnDivider = 300;
-    // this.ignoreClamp = true;
+    this.pierceDamage = 0.1;
+    this.turnDivider = 300;
+    this.ignoreClamp = true;
+	this.patternTimer = this.patternMax;
+    this.pattern = 3;
+    this.rotateSpeed = Math.PI / 240;
 	
-    // // Wing sprites
-    // this.preChildren.push(
-		// new Sprite('bossDragonLeftWing', 278, -66).child(this, true),
-		// new Sprite('bossDragonRightWing', -278, -66).child(this, true)
-	// );
+    // Specific values
+    this.fireball = new Sprite('Fireball', 0, 550);
+    this.preChildren.push(
+        this.fireball,
+        new Sprite('hydraRoyalWingLeft', 0, 0),
+        new Sprite('hydraRoyalWingRight', 0, 0)
+    );
+    
+    // Movement pattern
+    this.movement = movement.pads;
+    
+    var damageScale = Boss.sum();
+	this.damageScale = damageScale;
+    
+	// Attack pattern 0 - Missile Barrage
+	this.setMovement(0, movement.flying);
+	for (var i = 0; i < 2; i++) {
+		var m = i * 2 - 1;
+		for (var j = 0; j < 2; j++) {
+			this.addWeapon(weapon.gun, {
+				sprite: 'rocket',
+				damage: 2 * damageScale,
+				range: 600,
+				rate: 75,
+				dx: m * (400 + 340 * j),
+				dy: 100 - 100 * j,
+				speed: 16,
+                target: Robot.PLAYER
+                //                                 args: [type,   radius, knockback
+                templates: [{ name: 'setupRocket', args: ['Enemy', 100,   150] }]
+			}, 0);
+		}
+	}
+	
+	// Attack pattern 1 - Hyper Beam
+	this.setMovement(1, movement.rotate);
+    this.hyperBeamData = {
+        sprite: 'bossCannon',
+        damage: damageScale,
+        rate: 60,
+        range: 1000,
+        discharge: 0.1,
+        duration: 480,
+        dy: 550,
+        cd: 60,
+		pierce: true,
+        target: Robot.PLAYER
+    };
+	this.addWeapon(wepaon.rail, this.hyperBeamData, 1);
+	
+	// Attack pattern 2 - Spawn baby
+	this.setMovement(2, movement.flyCenter);
+	this.addWeapon(weapon.spawn, {
+		enemies: [1, 0, HydraBoss],
+        max    : 2,
+        rate   : 120,
+        delay  : 120,
+        dx     : 0,
+        dy     : 0,
+        range  : 9999
+	}, 2);
+	
+	// Attack pattern 3 - Lay turrets
+	this.setMovement(3, movement.pads);
+	
+	// Attack pattern 4 - Fireball
+	this.setMovement(4, movement.flying);
+	this.addWeapon(weapon.gun, {
+		sprite: 'FireBall',
+		damage: 5 * damageScale,
+		range: 500,
+		radius: 200,
+		knockback: 100,
+		rate: 120,
+		dx: 0,
+		dy: 550,
+		rotation: -Math.PI / 20,
+		speed: 20,
+		angleOffset: 0,
+		target: Robot.PLAYER,
+        //                                 args: [type,    radius, knockback
+        templates: [{ name: 'setupRocket', args: ['Enemy', 200,    100] }]
+	}, 4);
+	
+    // Hydra's tail   
+	enemy.tail = new RopeTail(enemy, GetImage('hydraRoyalTail'), GetImage('hydraRoyalEnd'), 7, 175, 150, 175, 20);
+	enemy.ApplyDraw = function() {
+	
+		// Turret laying stops when all are occupied
+		if (this.pattern == 3 && gameScreen.enemyManager.turrets.length == 4) {
+			this.pattern = 0;
+			this.ApplyMove = this.movements[0];
+		}
+		
+		// Hyper beam
+		if (this.pattern == 1) 
+		{
+			for (var i = 0; i < playerManager.players.length; i++)
+			{
+				var r = playerManager.players[i].robot;
+				var dx = r.x - this.x;
+				var dy = r.y - this.y;
+				if (dx * dx + dy * dy < Sq(600))
+				{
+					var d = Math.sqrt(dx * dx + dy * dy);
+					dx /= d;
+					dy /= d;
+					r.Knockback(dx * 100, dy * 100);
+				}
+			}
+		}
+		if (this.hyperBeamData.cd < 0 && !this.firinLazors) {
+			this.firinLazors = true;
+		}
+		else if (this.hyperBeamData.cd > 0 && this.firinLazors) {
+			this.firinLazors = false;
+			this.pattern = 0;
+			this.ApplyMove = this.movements[0];
+			this.patternTimer = this.patternMax;
+		}
+	
+        // Tail
+        this.tail.update();
+        
+        canvas.save();
+        canvas.translate(this.sprite.width / 2, this.sprite.height / 2);
+		canvas.transform(this.sin, -this.cos, this.cos, this.sin, 0, 0);
+        
+		// Fireball charging
+		if (this.pattern == 4) {
+			var cd = this.patterns[4][0].cd;
+			if (cd < 90) {
+				var w = (1 - cd / 90) * this.fireball.width;
+				canvas.save();
+				canvas.translate(0, 550);
+				canvas.rotate(cd * Math.PI / 20);
+				canvas.drawImage(this.fireball, -w/2, -w/2, w, w);
+				canvas.restore();
+			}
+		}
+		else this.patterns[4][0].cd = this.patterns[4][0].rate;
+		
+		canvas.translate(-this.sprite.width / 2, -this.sprite.height / 2);
+		
+        // Wings
+        canvas.drawImage(this.rightWing, this.sprite.width - 250, -325);
+        canvas.drawImage(this.leftWing, 250 - this.leftWing.width, -325);
+        
+        canvas.restore();
+	}
+    
+    // Hydra's heads
+    enemy.head = new RopeTail(enemy, GetImage('hydraRoyalNeck'), GetImage('hydraRoyalHeadLarge'), 3, 125, 150, 0, 0);
+    enemy.headLeft = new RoyalHydraSideHead(enemy, new RopeTail(enemy, GetImage('hydraRoyalNeck'), GetImage('hydraRoyalHeadSmall'), 4, 125, 250, 0, 20), damageScale);
+    enemy.headRight = new RoyalHydraSideHead(enemy, new RopeTail(enemy, GetImage('hydraRoyalNeck'), GetImage('hydraRoyalHeadSmall'), 4, 125, 250, 50, 20), damageScale);
+    enemy.head.reverse = enemy.headLeft.rope.reverse = enemy.headRight.rope.reverse = true;
+    enemy.headLeft.rope.rel = Vector(COS_60, SIN_60);
+    enemy.headRight.rope.rel = Vector(COS_60, -SIN_60);
+    enemy.head.held = null;
+    enemy.head.heldTimer = 0;
+    enemy.head.hydra = enemy;
+    enemy.head.consumeDamageStart = damageScale;
+    enemy.head.consumeDamageTick = damageScale * 0.02;
+    enemy.head.consume = ConsumeAttackHelper;
+    enemy.ApplySprite = function() {
+        var end = this.head.segments[this.head.segments.length - 1];
+        this.head.update();
+        this.headLeft.update();
+		this.headRight.update();
+        
+        // Consumption attack
+        var rot = this.head.getEndDir();
+		rot.Rotate(-1, 0);
+		this.head.cos = rot.x;
+		this.head.sin = rot.y;
+        this.head.x = end.pos.x;
+        this.head.y = end.pos.y;
+        this.head.consume();
+    }
 
-    // // Movement pattern
-    // this.movement = movement.flying;
+    return enemy;
+}
 
-    // var damageScale = Boss.sum();
+function RoyalHydraSideHead(hydra, rope, damage) {
 
-    // // Attack pattern 0 - Lasers and fire
-    // this.setMovement(0, movement.flying);
-    // for (var i = 0; i < 2; i++) {
-        // this.addWeapon(weapon.gun, {
-            // sprite: 'bossLaser',
-            // damage: 0.1 * damageScale,
-            // range : 1000,
-            // rate  : 30,
-            // dx    : -185 + 370 * i,
-            // dy    : 17,
-            // speed : 15,
-            // pierce: true
-        // });
-    // }
-    // this.addWeapon(weapon.gun, {
-        // sprite  : 'bossFlame',
-        // damage  : 0.1 * damageScale,
-        // range   : 300,
-        // rate    : 3,
-        // dx      : 0,
-        // dy      : 100,
-        // onUpdate: projEvents.fireUpdate
-    // });
+	this.hydra = hydra;
+	this.rope = rope;
+	this.pattern = 0;
+    this.held = null;
+    this.heldTimer = 0;
+    this.consumeDamageStart = damage * 5;
+    this.consumeDamageTick = damage;
+    this.consume = ConsumeAttackHelper;
+	
+	// Side head weapon 0 - flamethrower
+	this.flamethrower = EnemyWeaponRail;
+	this.fireData = {
+		subWeapon: EnemyWeaponFire,
+		damage: 0.1 * damage,
+        range: 400,
+        interval: 3,
+        dx: 120,
+        dy: 0,
+		discharge: 0,
+        duration: 240,
+		rate: 120,
+        speed: 14,
+		cd: 0,
+		list: gameScreen.enemyManager.bullets
+	};
+}
 
-    // // Attack pattern 1 - Spawning minibosses
-    // this.setMovement(1, movement.flyCenter);
-    // this.addWeapon(weapon.spawn, {
-        // enemies: DRAGON_SPAWNS,
-        // max    : 8,
-        // rate   : 120,
-        // delay  : 300,
-        // dx     : 0,
-        // dy     : -100,
-        // range  : 9999
-    // }, 1);
+RoyalHydraSideHead.prototype.IsInRange = function() { return true; }
 
-    // // Attack pattern 2 - Homing missiles
-    // this.setMovement(2, movement.dragon);
-    // this.addWeapon(weapon.gun, {
-		// sprite: 'bossFlame',
-        // damage: 0.05 * damageScale,
-        // range : 300,
-        // rate  : 3,
-        // dx    : 0,
-        // dy    : 42,
-		// onUpdate: projEvents.fireUpdate
-    // }, 2);
-    // for (var i = 0; i < 2; i++) {
-        // this.addWeapon(weapon.gun, {
-            // sprite   : 'rocket',
-            // damage   : 5 * damageScale,
-            // range    : 600,
-            // radius   : 100,
-            // knockback: 150,
-            // rate     : 90,
-            // dx       : -238 + 476 * i,
-            // dy       : -10,
-            // speed    : 8,
-			// templates: [
-				// //                     args: [type,    radius, knockback]
-				// { name: 'setupRocket', args: ['Enemy', 100,    150] },
-				// //                     args: [target,       rotSpeed]
-				// { name: 'setupHoming', args: [Robot.PLAYER, 0.02] }
-			// ]
-        // }, 2);
-    // }
+RoyalHydraSideHead.prototype.update = function() {
+	if (!gameScreen.paused) {
+		this.rope.turnTowards(this.hydra.head.dir, 0.04, ROPE_TURN_END);
+		this.rope.followParent();
+		
+		var end = this.rope.segments[this.rope.segments.length - 1];
+		this.x = end.pos.x;
+		this.y = end.pos.y;
+		var rot = this.rope.getEndDir();
+		rot.Rotate(-1, 0);
+		this.cos = rot.x;
+		this.sin = rot.y;
+		this.angle = Math.atan2(this.sin, this.cos);
+        
+        this.consume();
+        
+        if (!this.held) {
+            this.flamethrower(this.fireData);
+        }
+	}
+	this.rope.update();
+}
 
-	// // The enemy's tail
-    // this.tail = new RopeTail(
-        // /* Robot      */ this,
-        // /* Segment    */ 'bossDragonEnd',
-        // /* End        */ 'bossDragonEnd',
-        // /* Length     */ 5,
-        // /* Offset     */ 150,
-        // /* Base       */ 0,
-        // /* End Offset */ 0,
-        // /* Constraint */ 45,
-        // /* Front      */ true
-    // );
-    // this.tail.setTurrets(
-        // /* Sprite */ 'bossDragonTurret',
-        // /* DX     */ 0,
-        // /* DY     */ 0,
-        // /* Weapon */ {
-            // damage: damageScale,
-            // range : 1000,
-            // rate  : 60,
-            // dx    : 0,
-            // dy    : 48,
-            // target: Robot.PLAYER
-        // }
-    // );
-// }
+function ConsumeAttackHelper() {
 
-// /**
- // * Updates and draws the tail before the dragon is drawn
- // */
-// DragonBoss.prototype.onPreDraw = function() {
-    // this.tail.update();
-// };
+    // Consume attack
+    var holdX = this.x + this.cos * 150;
+    var holdY = this.y + this.sin * 150;
+    if (!this.held && this.hydra.pattern != 1 && this.heldTimer <= 0) {
+        var p = playerManager.getClosest(this.x, this.y);
+        var dx = p.x - holdX;
+        var dy = p.y - holdY;
+        if (dx * dx + dy * dy < 10000) {
+            this.held = p;
+            this.heldTimer = 300;
+            p.disableClamp = true;
+            p.Damage(this.consumeDamageStart, this.hydra);
+        }
+    }
+    else if (this.held) {
+        //this.held.Damage(this.consumeDamageTick, this.hydra);
+        this.held.x = holdX;
+        this.held.y = holdY;
+        this.heldTimer--;
+        if (this.heldTimer <= 0 || this.hydra.pattern == 1) {
+            this.heldTimer = 300;
+            this.held.disableClamp = false;
+            this.held = null;
+        }
+    }
+    else if (this.heldTimer > 0) {
+        this.heldTimer--;
+    }
+}
